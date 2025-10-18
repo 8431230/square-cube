28ac31827ad5956db7c9961ae30ff9dc02316765
Soumya <bangaragundsoumya@gmail.com>
2025-10-18 12:50:28 +0530

    save

---
+++ modifysquare.py
@@ -0,0 +1,7 @@
+number = int(input("Enter first number:"))
+number = int(input("Enter second number:"))
+for number in range (1,10):
+    square=number*2
+    cube=number*number*number
+    print("square of{number}is {square}:")
+    print("cube of{number} is {cube}:code")
